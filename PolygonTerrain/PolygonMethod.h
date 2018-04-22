a748d548dfe9711352090b7f6f73be8f3a0d4e3a
hkskyp <hkskyp@gmail.com>
2018-04-23 00:47:45 +0900

    sutherland hodgman 알고리즘 추가

--- PolygonTerrain/PolygonMethod.h
+++ PolygonTerrain/PolygonMethod.h
@@ -1,18 +1,24 @@
 #pragma once
 #include <vector>
-#include <windows.h>
 
-double DotProduct(POINT v1, POINT v2)
+class point2d
+{
+public:
+    int x;
+    int y;
+};
+
+double DotProduct(point2d v1, point2d v2)
 {
     return v1.x*v2.x + v1.y*v2.y;
 }
 
-double CrossProduct(POINT v1, POINT v2)
+double CrossProduct(point2d v1, point2d v2)
 {
     return v1.x*v2.y - v1.y*v2.x;
 }
 
-int IsWindingDirection(const std::vector<POINT>& polygon)
+int IsWindingDirection(const std::vector<point2d>& polygon)
 {
     if (polygon.empty()) { return 0; }
 
@@ -32,4 +38,20 @@ int IsWindingDirection(const std::vector<POINT>& polygon)
 
     // Collinear 
     return 0;
+}
+
+point2d intersection_point(const point2d& p1, const point2d& p2, const point2d& p3, const point2d& p4)
+{
+    const int a1 = (p2.y - p1.y);
+    const int b1 = (p1.x - p2.x);
+    const int c1 = (-a1 * p1.x - b1 * p1.y);
+
+    const int a2 = (p4.y - p3.y);
+    const int b2 = (p3.x - p4.x);
+    const int c2 = (-a2 * p3.x - b2 * p3.y);
+
+    const float ratio = 1.0f / (b2 * a1 - b1 * a2);
+
+    return { static_cast<int>((b1 * c2 - b2 * c1) * ratio), static_cast<int>((a2 * c1 - a1 * c2) * ratio) };
+
 }
