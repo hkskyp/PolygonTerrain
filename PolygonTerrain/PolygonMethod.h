aecef8790ca87bf09e7728a927e44d75e0e44a8c
hkskyp <hkskyp@gmail.com>
2018-04-23 21:53:40 +0900

    SutherlandHodgman 알고리즘 추가

--- PolygonTerrain/PolygonMethod.h
+++ PolygonTerrain/PolygonMethod.h
@@ -4,9 +4,14 @@
 class point2d
 {
 public:
+    point2d(int _x, int _y) : x(_x), y(_y) {}
     int x;
     int y;
 };
+
+const int oriCW  = 1;
+const int oriCCW = -1;
+const int oriCollinear = 0;
 
 double DotProduct(point2d v1, point2d v2)
 {
@@ -18,7 +23,7 @@ double CrossProduct(point2d v1, point2d v2)
     return v1.x*v2.y - v1.y*v2.x;
 }
 
-int IsWindingDirection(const std::vector<point2d>& polygon)
+int IsOrientation(const std::vector<point2d>& polygon)
 {
     if (polygon.empty()) { return 0; }
 
@@ -32,12 +37,12 @@ int IsWindingDirection(const std::vector<point2d>& polygon)
     }
 
     // CW
-    if (sum > 0.0) { return  1; }
+    if (sum > 0.0) { return  oriCW; }
     // CCW
-    if (sum < 0.0) { return -1; }
+    if (sum < 0.0) { return oriCCW; }
 
     // Collinear 
-    return 0;
+    return oriCollinear;
 }
 
 point2d intersection_point(const point2d& p1, const point2d& p2, const point2d& p3, const point2d& p4)
@@ -54,4 +59,44 @@ point2d intersection_point(const point2d& p1, const point2d& p2, const point2d& 
 
     return { static_cast<int>((b1 * c2 - b2 * c1) * ratio), static_cast<int>((a2 * c1 - a1 * c2) * ratio) };
 
+}
+
+void SutherlandHodgman(std::vector<point2d>& clip_boundry, std::vector<point2d>& polygon)
+{
+    auto cn = clip_boundry.size();
+    auto pn = polygon.size();
+
+    std::vector<point2d> clip;
+    for (size_t i = 0; i < cn; ++i)
+    {
+        auto e1 = clip_boundry[(i + 0) % cn];
+        auto e2 = clip_boundry[(i + 1) % cn];
+        int a1 = (e2.y - e1.y);
+        int b1 = (e1.x - e2.x);
+        int c1 = (-a1 * e1.x - b1 * e1.y);
+
+        auto p2 = polygon.back();
+        for (auto& p1 : polygon)
+        {
+            bool in1 = (a1*p1.x + b1 * p1.y + c1) < 0;
+            bool in2 = (a1*p2.x + b1 * p2.y + c1) < 0;
+
+            if (in1 && in2)
+            {
+                clip.emplace_back(p1);
+            }
+            else if (!in1 && in2)
+            {
+                clip.emplace_back(intersection_point(e1, e2, p1, p2));
+            }
+            else if (in1 && !in2)
+            {
+                clip.emplace_back(intersection_point(e1, e2, p2, p1));
+                clip.emplace_back(p1);
+            }
+
+            p2 = p1;
+        }
+        polygon = std::move(clip);
+    }
 }
--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -13,30 +13,30 @@ namespace UnitTest1
 		TEST_METHOD(determinePolygonCW)
 		{
 			// TODO: Your test code here
-            std::vector<POINT> cw;
-            POINT pt[] = { {-1, -1}, {0, 1}, {1, -1} };
+            std::vector<point2d> cw;
+            point2d pt[] = { {-1, -1}, {0, 1}, {1, -1} };
             cw.emplace_back(pt[0]);
             cw.emplace_back(pt[1]);
             cw.emplace_back(pt[2]);
-            Assert::IsTrue(IsWindingDirection(cw) == 1);
+            Assert::IsTrue(IsOrientation(cw) == 1);
 		}
 
 		TEST_METHOD(determinePolygonCCW)
 		{
 			// TODO: Your test code here
-            std::vector<POINT> ccw;
-            POINT pt[] = { {-1, -1}, {1, -1}, {0, 1} };
+            std::vector<point2d> ccw;
+            point2d pt[] = { {-1, -1}, {1, -1}, {0, 1} };
             ccw.emplace_back(pt[0]);
             ccw.emplace_back(pt[1]);
             ccw.emplace_back(pt[2]);
-            Assert::IsTrue(IsWindingDirection(ccw) == -1);
+            Assert::IsTrue(IsOrientation(ccw) == -1);
 		}
 
         TEST_METHOD(crossProduct)
         {
-            POINT p1 = {  0, 1 };
-            POINT p2 = {  1, 0 };
-            POINT p3 = { -1, 0 };
+            point2d p1 = {  0, 1 };
+            point2d p2 = {  1, 0 };
+            point2d p3 = { -1, 0 };
 
             Assert::IsTrue(CrossProduct(p1, p2) < 0.0); // right
             Assert::IsTrue(CrossProduct(p1, p3) > 0.0); // left
